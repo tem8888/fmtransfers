7d7d3423524428cfcf47356f623b547b553bec14
tem <tem.8888@yandex.ru>
2020-10-14 11:57:51 +0300

    upd

--- models/discordSendMessage.js
+++ models/discordSendMessage.js
@@ -9,8 +9,8 @@ const discordSendMessage = (bid, status) => {
 
     axios({
         method: 'post',
-      //  url: 'https://discordapp.com/api/webhooks/639209744195780620/Oxr4NBpM3186OVHcuT2tuozq8Z5o8cmICHRw88-xtk_hY0A3dMK1lFuqo-3bPt5N-OSZ',
-          url: 'https://discordapp.com/api/webhooks/762805897507373096/AKGxHCc4nVgJo7AlGEea-7dZBNn6p8uE6fU9jdSx_zG_p6v2hSpg0t9D8Zq1Tw7yNj7s',
+        url: 'https://discordapp.com/api/webhooks/639209744195780620/Oxr4NBpM3186OVHcuT2tuozq8Z5o8cmICHRw88-xtk_hY0A3dMK1lFuqo-3bPt5N-OSZ',
+      //    url: 'https://discordapp.com/api/webhooks/762805897507373096/AKGxHCc4nVgJo7AlGEea-7dZBNn6p8uE6fU9jdSx_zG_p6v2hSpg0t9D8Zq1Tw7yNj7s',
         headers: {"Content-Type": "application/json"},
         data: params
     }).then(() => {
