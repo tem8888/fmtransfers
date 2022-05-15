59323801bc7b11c6244b250be4e6e3c8e5fc79cc
Artem Dmytrishyn <gamegir@gmail.com>
2022-05-15 13:14:55 +0300

    minor styles fixes for Auth component

--- client/src/components/Auth/AuthSuccess.js
+++ client/src/components/Auth/AuthSuccess.js
@@ -2,10 +2,6 @@ import React from 'react'
 
 import {connect} from 'react-redux'
 const {logout, loadUser} = require('../../store/actions/authActions.js')
-// const images = {
-// 	Strømsgodset: require('../../assets/img/Strom.png'), 
-//   Roma: require('../../assets/img/Roma.png'),
-//   'Nashe Pivo': require('../../assets/img/NashePivo.png')}
 
 const AuthSuccess = ({auth, loadUser, logout}) => {
 
@@ -17,25 +13,17 @@ const AuthSuccess = ({auth, loadUser, logout}) => {
 	return (
 		!auth.isLoading ?
 
-			<div className="auth__succes valign-wrapper" 
-		// style={{
-		//   height: '80px',
-		//   background: `url(${images[club]})`,
-		//   backgroundSize: 'contain',
-		//   backgroundRepeat: 'no-repeat',
-		//   backgroundPosition: '50% 0%'
-		//}}
-		>
+			<div className="auth__succes valign-wrapper" >
 
-			<div className="col s6 center-align">
-				Hello, {auth.user.username}
-				<button className="waves-effect waves-light btn-small btn-logout" type="submit" onClick={submitLogoutHandler}>Logout</button>
+				<div className="col s6 center-align">
+					Hello, {auth.user.username}
+					<button className="waves-effect waves-light btn-small btn-logout" type="submit" onClick={submitLogoutHandler}>Logout</button>
+				</div>
+				<h6 className="col s6 center-align">
+					<div>{auth.user.club}</div>
+					<div className="auth__money">{auth.user.money.toFixed(2)}</div>
+				</h6>
 			</div>
-			<h6 className="col s6 center-align">
-						<div>{auth.user.club}</div>
-						<div style={{marginTop: '0.8rem', fontSize: '1.5rem'}}>{auth.user.money.toFixed(2)}</div>
-					</h6>
-		</div>
 
 		:
 		null
