cd2980401c9a24411d4d55e761f5129674d5f9df
Artem Dmytrishyn <gamegir@gmail.com>
2022-06-01 13:28:51 +0300

    delete unused files and move NavLink component

---
+++ client/src/components/NavLinks/NavLinks.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { NavLink } from "react-router-dom"
+import './navlinks.css'
+
+const NavLinks = ({auth, playerList, shortList, squadlist}) => {
+  console.log("NavLinks")
+  return (
+    <ul className="tabs tabs-fixed-width">
+      <li className="tab">
+        <NavLink to='/transfers' activeClassName='active'>
+          Трансферный список ({playerList.length})
+        </NavLink>
+      </li>
+      <li className="tab">
+        <NavLink to='/shortlist' activeClassName='active'>
+          Мой список ({auth.isAuthenticated && shortList.length})
+        </NavLink>
+      </li>
+      <li className="tab">
+        <NavLink to='/squad' activeClassName='active'>
+          Мой состав ({auth.isAuthenticated && squadlist.length})
+        </NavLink>
+      </li>
+    </ul>
+  )
+}
+
+const mapStateToProps = state => ({
+	auth: state.auth,
+  squadlist: state.squadList.filtered,
+	playerList: state.playersList.filtered,
+	shortList: state.shortState.list
+})
+
+
+export default connect(mapStateToProps)(NavLinks)
