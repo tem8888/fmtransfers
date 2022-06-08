92334975ce4122c7e0c83d2c1e469ba36102de6e
Artem Dmytrishyn <gamegir@gmail.com>
2022-06-08 17:06:14 +0300

    add static data

---
+++ client/src/assets/data/select-options.js
@@ -0,0 +1,29 @@
+export const preferredFootOptions = [
+	{label: 'Any', value: ''},
+	{label: 'Left', value: 'left'},
+	{label: 'Right', value: 'right'},
+	{label: 'Either', value: 'either'},
+]
+
+export const positionOptions = [
+	{label: 'Any', value: ''},
+	{label: 'Goalkeaper', value: 'gk'},
+	{label: 'Right Defender', value: 'dr'},
+	{label: 'Central Defender', value: 'dc'},
+	{label: 'Left Defender', value: 'dl'},
+	{label: 'Right Wing-Back', value: 'wbr'},
+	{label: 'Left Wing-Back', value: 'wbl'},
+	{label: 'Defensive Midfielder', value: 'dm'},
+	{label: 'Central Midfield', value: 'mc'},
+	{label: 'Right Midfielder', value: 'mr'},
+	{label: 'Left Midfielder', value: 'ml'},
+	{label: 'Central Attacking Midfielder', value: 'amc'},
+	{label: 'Right Attacking Midfielder', value: 'amr'},
+	{label: 'Left Attacking Midfielder', value: 'aml'},
+	{label: 'Striker', value: 'stc'},
+]
+export const wpneededOptions = [
+	{label: 'Any', value: ''},
+	{label: 'Needed', value: 'yes'},
+	{label: 'Not Needed', value: 'no'},
+]
---
+++ client/src/assets/data/skills.js
@@ -0,0 +1,68 @@
+export const gk = [
+    { name: 'Ввод мяча', id: 'thr' },
+    { name: 'Взаимодействие', id: 'com' },
+    { name: 'Выбивание',  id: 'kic' },
+    { name: 'Игра в штрафной', id: 'cmd' },
+    { name: 'Игра руками',  id: 'tro' },
+    { name: 'Один на один', id: 'han' },
+    { name: 'Частота отбивания',      id: 'ovo' },
+    { name: 'Частота отбивания', id: 'pun' },
+    { name: 'Пас',   id: 'pasgk' },
+    { name: 'Первое касание',      id: 'firgk' },
+    { name: 'Реакция', id: 'ref' },
+    { name: 'Игра в воздухе',   id: 'aer' },
+    { name: 'Эксцентричность',   id: 'ecc' }
+]
+
+export const technique = [
+    { name: 'Вброс из-за боковой', id: 'lth' },
+    { name: 'Дальние удары', id: 'lon' },
+    { name: 'Дриблинг',  id: 'dri' },
+    { name: 'Завершение атаки', id: 'fin' },
+    { name: 'Игра головой',  id: 'hea' },
+    { name: 'Штрафные', id: 'fre' },
+    { name: 'Навесы',      id: 'cro' },
+    { name: 'Опека', id: 'mar' },
+    { name: 'Отбор',   id: 'tck' },
+    { name: 'Пас',      id: 'pas' },
+    { name: 'Пенальти', id: 'pen' },
+    { name: 'Первое касание',   id: 'fir' },
+    { name: 'Техника',   id: 'tec' },
+    { name: 'Угловые',   id: 'cor' }
+]
+
+export const mental = [
+    { name: 'Агрессивность', id: 'agg' },
+    { name: 'Видение поля', id: 'vis' },
+    { name: 'Выбор позиции',  id: 'pos' },
+    { name: 'Игра без мяча', id: 'otb' },
+    { name: 'Импровизация',  id: 'fla' },
+    { name: 'Интуиция', id: 'ant' },
+    { name: 'Командная игра',      id: 'tea' },
+    { name: 'Концентрация', id: 'cnt' },
+    { name: 'Лидерство',   id: 'ldr' },
+    { name: 'Принятие решений',      id: 'dec' },
+    { name: 'Работоспособность', id: 'wor' },
+    { name: 'Решительность',   id: 'det' },
+    { name: 'Самообладание',   id: 'cmp' },
+    { name: 'Храбрость',   id: 'bra' }
+]
+
+export const physics = [
+    { name: 'Выносливость', id: 'sta' },
+    { name: 'Высота прыжка',      id: 'jum' },
+    { name: 'Координация', id: 'bal' },
+    { name: 'Ловкость',   id: 'agi' },
+    { name: 'Природные данные',      id: 'nat' },
+    { name: 'Сила', id: 'str' },
+    { name: 'Скорость',   id: 'pac' },
+    { name: 'Ускорение',   id: 'acc' }
+]
+
+export const hidden = [
+    { name: 'Амбиции', id: 'amb' },
+    { name: 'Ключевые матчи',      id: 'impm' },
+    { name: 'Профессионализм', id: 'prof' },
+    { name: 'Травматизм',   id: 'injpr' },
+    { name: 'Стабильность',      id: 'cons' }
+]
