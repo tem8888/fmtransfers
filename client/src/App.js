e21452557002f5483196863393e12e2fdd2c5a9c
Artem Dmytrishyn <gamegir@gmail.com>
2022-06-08 17:13:37 +0300

    move initial fetch to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,37 @@
-import React from 'react'
-import { BrowserRouter as Router } from "react-router-dom"
+import React, {useEffect} from 'react'
+import { BrowserRouter } from "react-router-dom"
+import { connect } from 'react-redux'
 import PlayerProfile from './components/PlayerProfile/PlayerProfile.js'
 import SearchForm from './components/SearchForm/SearchForm.js'
 import Auth from './components/Auth/Auth.js'
 import Lists from './components/Lists/Lists.js'
-import NavLinks  from './components/NavLinks/NavLinks.js'
+import { initialFetch } from './store/actions'
 
+function App({initialFetch}) {
 
-function App() {
+	useEffect(() => {
+		initialFetch()
+	},[initialFetch])
 
   return (
-	<Router history={Router.browserHistory} basename="/">
-		<div className="App">
-			<div className="container">
-				<div className="row">
-					<div className="col l3 m12 s12">
-						<Auth/> 
-						<SearchForm />
-					</div>
-					<div className="col l9 m12 s12">
-						<NavLinks />
-						<Lists />
-					</div>
+	<div className="App">
+		<div className="container">
+			<div className="row">
+				<div className="col l3 m12 s12">
+					<Auth/> 
+					<SearchForm />
 				</div>
-				<div className="row">
-				<PlayerProfile/>   
+				<div className="col l9 m12 s12">
+					<BrowserRouter history={BrowserRouter.browserHistory} basename="/">
+						<Lists />
+					</BrowserRouter>
 				</div>
 			</div>
-		</div>		
-	</Router>
+			<div className="row">
+			<PlayerProfile/>   
+			</div>
+		</div>
+	</div>		
 )}
 
-export default App
+export default connect(null, { initialFetch })(App)
--- client/src/components/Lists/Lists.js
+++ client/src/components/Lists/Lists.js
@@ -1,35 +1,31 @@
-import React, {useEffect} from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 import { Switch, Route, Redirect } from "react-router-dom"
 import PlayersList from './PlayersList.js'
 import Shortlist from './Shortlist.js';
 import SquadList from './SquadList.js';
-import { initialFetch } from '../../store/actions'
+import NavLinks  from '../NavLinks/NavLinks.js'
 
-const Lists = ({ initialFetch }) => {
-
-	useEffect(() => {
-		initialFetch()
-	},[initialFetch])
+const Lists = () => {
 
   	return (
-	<>
-	<div className="table-container">
-		<Switch>
-			<Route exact path='/transfers'>
-				<PlayersList />  
-			</Route>
-			<Route path='/shortlist'>
-				<Shortlist /> 
-			</Route>
-			<Route path='/squad'>
-				<SquadList /> 
-			</Route>  
-			<Redirect to="/transfers" /> {/* Redirects all unknown paths to `/transfers` */}
-		</Switch>
-	</div>
-	</>
+		<>
+		<NavLinks />
+		<div className="table-container">
+			<Switch>
+				<Route exact path='/transfers'>
+					<PlayersList />  
+				</Route>
+				<Route path='/shortlist'>
+					<Shortlist /> 
+				</Route>
+				<Route path='/squad'>
+					<SquadList /> 
+				</Route>  
+				<Redirect to="/transfers" /> {/* Redirects all unknown paths to `/transfers` */}
+			</Switch>
+		</div>
+		</>
 	)
 }
 
-export default connect(null, { initialFetch })(Lists)
+export default Lists
