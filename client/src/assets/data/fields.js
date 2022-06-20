35d78a7ecd66755d1461e358ac4c2ea9a428e320
Artem Dmytrishyn <gamegir@gmail.com>
2022-06-20 21:07:42 +0300

    fix wrong field name in data array

---
+++ client/src/assets/data/fields.js
@@ -0,0 +1,9 @@
+export const minmaxFields = [
+	'ca', 'pa', 'age', 'price',
+	'lth', 'lon', 'dri', 'fin', 'hea', 'fre', 'cro', 'mar', 'tck', 'pas', 'pen', 'fir', 'tec', 'cor', 'agg', 'vis', 'pos', 'otb', 'fla', 'ant', 'tea', 'cnt', 'lead', 'dec', 'wor', 'det', 'cmp', 'bra', 'sta', 'jum', 'bal', 'agi', 'nat', 'str', 'pace', 'acc', 'amb', 'impm', 'prof', 'injpr', 'cons', 'thr', 'com', 'kic', 'cmd', 'tro', 'han', 'ovo', 'pun', 'ref', 'aer', 'ecc'
+]
+export const textFields = [
+	'name',
+	'preferredfoot',
+	'wpneeded',
+]
--- client/src/store/reducers/playerListReducer.js
+++ client/src/store/reducers/playerListReducer.js
@@ -1,4 +1,5 @@
 import convertPositions from '../../helpers/convertPositions'
+import { minmaxFields, textFields } from '../../assets/data/fields'
 import { 
 	PLAYERS_LOADED,
 	LOAD_PLAYERS_ERROR,
@@ -7,16 +8,6 @@ import {
 	SHOW_SQUAD_PLAYER,
 	SHOW_LIST_PLAYER,
 	} from '../actions/types'
-
-const minmaxFields = [
-	'ca', 'pa', 'age', 'price',
-	'lth', 'lon', 'dri', 'fin', 'hea', 'fre', 'cro', 'mar', 'tck', 'pas', 'pen', 'fir', 'tec', 'cor', 'agg', 'vis', 'pos', 'otb', 'fla', 'ant', 'tea', 'con', 'lead', 'dec', 'wor', 'det', 'cmp', 'bra', 'sta', 'jum', 'bal', 'agi', 'nat', 'str', 'pace', 'acc', 'amb', 'impm', 'prof', 'injpr', 'cons', 'thr', 'com', 'kic', 'cmd', 'tro', 'han', 'ovo', 'pun', 'ref', 'aer', 'ecc'
-]
-const textFields = [
-	'name',
-	'preferredfoot',
-	'wpneeded',
-]
 
 const initialState = {
 	initial: [],
